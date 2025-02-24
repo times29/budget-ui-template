1054d3cb5e4653945b501ef43365ca7eb3b10359
times29 <tds29@posteo.net>
2025-02-24 14:50:42 +0100

    Setup template

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...compat
+    .extends(
+      'plugin:@angular-eslint/recommended',
+      'plugin:@angular-eslint/template/process-inline-templates',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    )
+    .map(config => ({
+      ...config,
+      files: ['**/*.ts']
+    })),
+  {
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        project: ['tsconfig.json'],
+        createDefaultProgram: true
+      }
+    },
+
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 140
+        }
+      ],
+
+      '@angular-eslint/component-class-suffix': [
+        'error',
+        {
+          suffixes: ['Page', 'Component']
+        }
+      ],
+
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
+        }
+      ],
+
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
+      ],
+
+      '@typescript-eslint/member-ordering': [
+        'error',
+        {
+          default: ['field', 'constructor', 'method']
+        }
+      ]
+    }
+  },
+  ...compat.extends('plugin:@angular-eslint/template/recommended').map(config => ({
+    ...config,
+    files: ['**/*.html']
+  })),
+  {
+    files: ['**/*.html'],
+    rules: {}
+  }
+];
---
+++ src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { categoriesPath } from './category/category.routes';
+import { expensesPath } from './expense/expense.routes';
+
+export const loginPath = 'login';
+export const defaultPath = categoriesPath;
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: defaultPath,
+    pathMatch: 'full'
+  },
+  {
+    path: categoriesPath,
+    loadChildren: () => import('./category/category.routes')
+  },
+  {
+    path: expensesPath,
+    loadChildren: () => import('./expense/expense.routes')
+  },
+  {
+    path: '**',
+    redirectTo: defaultPath
+  }
+];
+export default appRoutes;
